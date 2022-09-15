a0d906eb9d3ed792b3504f3c777f413d42e715de
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-09-15 14:14:25 +1000

    FunctionK2TypeOps supports invariant types

--- src/main/g8/src/main/scala/com/ruchij/types/FunctionKTypes.scala
+++ src/main/g8/src/main/scala/com/ruchij/types/FunctionKTypes.scala
@@ -1,11 +1,13 @@
 package com.ruchij.types
 
-import cats.{Applicative, ApplicativeError, ~>}
+import cats.arrow.FunctionK
+import cats.data.Kleisli
+import cats.{Applicative, ApplicativeError, Functor, Monad, ~>}
 
 object FunctionKTypes {
-
-  implicit class FunctionK2TypeOps[F[+_, +_], A, B](value: F[B, A]) {
-    def toType[G[_], C >: B](implicit functionK: F[C, *] ~> G): G[A] = functionK(value)
+  implicit class FunctionK2TypeOps[F[_, _], A, B](value: F[B, A]) {
+    def toType[G[_], C >: B](implicit functionK: F[C, *] ~> G, functor: Functor[F[*, A]]): G[A] =
+      functionK.apply(functor.map(value)(identity[C]))
   }
 
   implicit def eitherToF[L, F[_]: ApplicativeError[*[_], L]]: Either[L, *] ~> F =
@@ -14,4 +16,15 @@ object FunctionKTypes {
         either.fold(ApplicativeError[F, L].raiseError, Applicative[F].pure)
     }
 
+  implicit def eitherLeftFunctor[R]: Functor[Either[*, R]] =
+    new Functor[Either[*, R]] {
+      override def map[A, B](fa: Either[A, R])(f: A => B): Either[B, R] = fa.left.map(f)
+    }
+
+  implicit def identityFunctionK[F[_]]: ~>[F, F] = FunctionK.id[F]
+
+  implicit class KleisliOption[F[_]: Monad, A, B](kleisli: Kleisli[F, A, Option[B]]) {
+    def or(other: => Kleisli[F, A, B]): Kleisli[F, A, B] =
+      kleisli.flatMap(_.fold(other)(value => Kleisli.pure[F, A, B](value)))
+  }
 }
