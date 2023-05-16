c4e9bfa5035208b182bf8cfa568e2975c254f99d
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-05-17 00:04:33 +1000

    Updating deprecated code

--- src/main/g8/src/test/scala/com/ruchij/test/mixins/MockedRoutes.scala
+++ src/main/g8/src/test/scala/com/ruchij/test/mixins/MockedRoutes.scala
@@ -3,6 +3,7 @@ package com.ruchij.test.mixins
 import cats.effect.kernel.Sync
 import com.ruchij.services.health.HealthService
 import com.ruchij.web.Routes
+import fs2.compression.Compression
 import org.http4s.HttpApp
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.OneInstancePerTest
@@ -12,8 +13,9 @@ trait MockedRoutes[F[_]] extends MockFactory with OneInstancePerTest {
   val healthService: HealthService[F] = mock[HealthService[F]]
 
   val sync: Sync[F]
+  val compression: Compression[F]
 
   def createRoutes: HttpApp[F] =
-    Routes[F](healthService)(sync)
+    Routes[F](healthService)(sync, compression)
 
 }
--- src/main/g8/src/test/scala/com/ruchij/test/mixins/io/MockedRoutesIO.scala
+++ src/main/g8/src/test/scala/com/ruchij/test/mixins/io/MockedRoutesIO.scala
@@ -3,7 +3,10 @@ package com.ruchij.test.mixins.io
 import cats.effect.IO
 import cats.effect.kernel.Sync
 import com.ruchij.test.mixins.MockedRoutes
+import fs2.compression.Compression
 
 trait MockedRoutesIO extends MockedRoutes[IO] {
   override val sync: Sync[IO] = IO.asyncForIO
+
+  override val compression: Compression[IO] = Compression.forIO
 }
