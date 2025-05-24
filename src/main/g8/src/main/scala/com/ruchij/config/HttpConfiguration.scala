11208a652ee13a919fa90ef6f48364770e860919
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-05-24 19:31:21 +1000

    Make `allowedOriginHosts` a `val` instead of a `def`
    Changed `allowedOriginHosts` from a method (`def`) to a value (`val`) to improve performance by avoiding repeated computation. This ensures the set is computed only once and reused across calls.

--- src/main/g8/src/main/scala/com/ruchij/config/HttpConfiguration.scala
+++ src/main/g8/src/main/scala/com/ruchij/config/HttpConfiguration.scala
@@ -3,5 +3,5 @@ package com.ruchij.config
 import com.comcast.ip4s.{Host, Port}
 
 final case class HttpConfiguration(host: Host, port: Port, private val allowedOrigins: Option[Set[String]]) {
-  def allowedOriginHosts: Set[String] = allowedOrigins.getOrElse(Set.empty)
+  val allowedOriginHosts: Set[String] = allowedOrigins.getOrElse(Set.empty)
 }
