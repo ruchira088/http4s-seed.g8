72d509e721addc5012ee85e14679ef1fe218e5e8
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-05-24 19:19:59 +1000

    Refactor string list ConfigReader to support generic iterables
    Replaced the specific ConfigReader for Set[String] with a generic implementation for any iterable type. This change improves flexibility and reusability when parsing comma-separated strings into different collection types.

--- src/main/g8/src/main/scala/com/ruchij/config/ConfigReaders.scala
+++ src/main/g8/src/main/scala/com/ruchij/config/ConfigReaders.scala
@@ -3,11 +3,19 @@ package com.ruchij.config
 import com.comcast.ip4s.{Host, Port}
 import pureconfig.ConfigReader
 
+import scala.collection.Factory
+
 object ConfigReaders {
   implicit val hostConfigReader: ConfigReader[Host] = ConfigReader.fromNonEmptyStringOpt(Host.fromString)
 
   implicit val portConfigReader: ConfigReader[Port] = ConfigReader.fromNonEmptyStringOpt(Port.fromString)
 
-  implicit val stringListConfigReader: ConfigReader[Set[String]] =
-    ConfigReader.fromString(stringValue => Right(stringValue.split(",").map(_.trim).filter(_.nonEmpty).toSet))
+  implicit def stringIterableConfigReader[Itr[x] <: IterableOnce[x]](
+    implicit factory: Factory[String, Itr[String]]
+  ): ConfigReader[Itr[String]] =
+    ConfigReader[String].map { string =>
+      factory.fromSpecific {
+        string.split(",").map(_.trim).filter(_.nonEmpty)
+      }
+    }
 }
